c909dec84970f571f98ea6351804b9832bf73d1d
ryghoul <runite02@gmail.com>
2025-10-22 02:10:52 -0700

    More stock

--- scripts/seed.js
+++ scripts/seed.js
@@ -27,16 +27,45 @@ const mongoose = require("mongoose");
     up("hoodie|Forest Green|Large", 4500, 8),
 
     /* Shirts */
-    up("hoodie|Forest Green|Small", 4500, 8),
-    up("hoodie|Forest Green|Medium", 4500, 8),
-    up("hoodie|Forest Green|Large", 4500, 8),
+    up("shirt|Black|XS", 2500, 8),
+    up("shirt|Black|S", 2500, 8),
+    up("shirt|Black|M", 2500, 8),
+    up("shirt|Black|L", 2500, 8),
+    up("shirt|Black|XL", 2500, 8),
 
+    up("shirt|White|XS", 2500, 8),
+    up("shirt|White|S", 2500, 8),
+    up("shirt|White|M", 2500, 8),
+    up("shirt|White|L", 2500, 8),
+    up("shirt|White|XL", 2500, 8),
+
+    up("shirt|FNF|XS", 2500, 8),
+    up("shirt|FNF|S", 2500, 8),
+    up("shirt|FNF|M", 2500, 8),
+    up("shirt|FNF|L", 2500, 8),
+    up("shirt|FNF|XL", 2500, 8),
+
+    up("shirt|V1|XS", 3000, 8),
+    up("shirt|V1|S", 3000, 8),
+    up("shirt|V1|M", 3000, 8),
+    up("shirt|V1|L", 3000, 8),
+    up("shirt|V1|XL", 3000, 8),
+
+    up("shirt|V2|XS", 3000, 8),
+    up("shirt|V2|S", 3000, 8),
+    up("shirt|V2|M", 3000, 8),
+    up("shirt|V2|L", 3000, 8),
+    up("shirt|V2|XL", 3000, 8),
 
     /* Totes */
     up("tote|Black|NOSIZE", 3500, 12),
+    up("tote|Green|NOSIZE", 3500, 12),
 
     /* Bandanas */
     up("bandana|Green Tea|NOSIZE", 2000, 30),
+    up("bandana|MORE TEA|NOSIZE", 2000, 30),
+    up("bandana|PERFORMATIVE|NOSIZE", 2000, 30),
+    up("bandana|REACH|NOSIZE", 2000, 30),
 
     /* Sticky Rice */
     up("stickyrice|STICKY RICE|NOSIZE", 800, 50),
